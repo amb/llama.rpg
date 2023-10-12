de8a6385a770829c4c7950dbc5afe81802664f92
Tommi Hyppänen <thyppane@gmail.com>
2023-10-12 11:18:14 +0300

    Functional.

--- main.py
+++ main.py
@@ -1,131 +1,135 @@
-from getch import getch
-from llm import llm
-from rewrite import mytho_compress
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.chains import LLMChain
+from langchain.llms import LlamaCpp
+from langchain.prompts import PromptTemplate
+from llama_cpp import LlamaGrammar
 
-one_line = 'root ::= [a-zA-Z\'",.;:!? ]+ "\n"'
+from getch import getch
+
+one_line = LlamaGrammar.from_string('root ::= [a-zA-Z\'",.;:!? ]+ "\n"', verbose=False)
+
 header = """
-The following is a text adventure game. It starts with <BEGIN> tag.
-After which there's a detailed description of the current environment and situation.
-You can type in any text you want after ">", and the game will try to act as a dungeon master based on it.
-The game contains various interesting locations, creatures and characters, which you can interact with.
+The following is a thrilling text adventure game set in a sci-fi universe. It starts with <BEGIN> tag.
+You are a dungeon master that leads the players through the story step by step.
+Give detailed descriptions of the current environment and situation. Don't leave things vague.
+Never fast forward in time without the player explicitly asking for it.
+Every action happens in real time.
 
 <BEGIN>
 """
 
+start_text = """
+Location: A dimly lit room.
+The soft, ambient hum of machinery fills the air. Mysterious symbols on a nearby control panel catch your attention.
+As you collect your thoughts, your hand brushes against a cold, metallic device in your pocket—it emits a faint, pulsating light.
+Your journey into the unknown begins.
 
-def choose(options):
-    a = " "
-    while a not in "123459c":
-        a = getch()
-        if a == "q":
-            return ("QUIT", None)
+> Look around
 
-    if a == "5":
-        res = ("INPUT", input("5. "))
-    elif a == "4" or a == "3" or a == "2" or a == "1":
-        res = ("INPUT", options[int(a) - 1])
-    elif a == "9":
-        res = ("REGEN", None)
-    elif a == "c":
-        res = ("SUMMARY", None)
-    else:
-        res = ("ERROR", None)
+"""
 
-    return res
+llm = LlamaCpp(
+    model_path="/mnt/Meteor/models/llm/mythomax-l2-13b.q5_K_M.gguf",
+    temperature=0.75,
+    max_tokens=256,
+    top_p=1.15,
+    repeat_penalty=1.15,
+    n_gpu_layers=44,
+    n_ctx=4096,
+    verbose=False,
+    # streaming=True,
+    # echo=True,
+)
 
 
-class Runner:
-    def __init__(self, initial_prompt):
-        self.prompt = initial_prompt
-        self.spent_context = 0
+class PromptManager:
+    def __str__(self):
+        return self.text
 
-    def _genjson(self, grm, stop=""):
-        return {
-            "prompt": self.prompt,
-            "n_predict": 256,
-            "stop": stop,
-            "ignore_eos": False,
-            "mirostat": 2,
-            "mirostat_tau": 8.0,
-            "mirostat_eta": 1.0,
-            "grammar": grm,
-        }
+    def __init__(self, prt=""):
+        assert isinstance(prt, str)
+        self.text = prt
 
-    def add(self, text, hide=False):
-        self.prompt += text
-        if not hide:
-            print(text, end="")
+    def add(self, text):
+        assert isinstance(text, str)
+        self.text += text
+        print(text, end="")
 
-    def rewind(self, location):
-        if location >= len(self.prompt):
-            return
-        count = len(self.prompt) - location
-        self.prompt = self.prompt[:-count]
-        print("\b" * count, end="")
+    def set(self, text):
+        assert isinstance(text, str)
+        self.text = text
 
-    def location(self):
-        return len(self.prompt)
+    def get(self):
+        return self.text
 
-    def generate(self, grm):
-        (result, result_info) = llm(self._genjson(grm))
-        self.spent_context = result_info["tokens_cached"]
-        self.add(result)
-        return result
 
-    def gen_options(self):
-        options = []
-        self.add("\nWhat would you like to do?\n\n")
-        for i in range(1, 5):
-            self.add(f"{i}.")
-            result = self.generate(one_line)
-            options.append(result[1:-1])
-        return options
+def gen_options(prompt):
+    options = []
+    prompt.add("\nWhat would you like to do?\n\n")
+    for i in range(1, 5):
+        prompt.add(f"{i}.")
+        result = llm(prompt.get(), grammar=one_line)
+        prompt.add(result)
+        options.append(result[1:-1])
+    return options
+
+
+def mytho_compress(text):
+    for _ in range(5):
+        compress = (
+            f"### Instruction:\nSummarize the following story in a few paragraphs.\n"
+            f"{text}"
+            f"\n### Response:\nCertainly. Here's the summary of the story in a few paragraphs:\n\nYou"
+        )
+
+        result = "You" + llm(compress)
+        rlen = llm.get_num_tokens(result)
+
+        # Repeat until something hopefully sensible is generated
+        if rlen > 5:
+            break
+
+    return result
 
 
 previous_choices = []
-total_story = [
-    "You wake up dizzy, not quite sure what is going on. Your head hurts and you feel like you've been unconscious for a long time.\n"
-]
-
-
-def build_story():
-    return header + "\n".join(total_story)
-
-
 checkpoint = 0
-runner = Runner(build_story())
+prompt = PromptManager(header + start_text)
+print(prompt, end="")
 while True:
-    start_prompt = runner.prompt
-    inner_story_piece = runner.generate(one_line)
-    total_story.append(inner_story_piece)
+    start_prompt = prompt.get()
+    prompt.add(llm(start_prompt, grammar=one_line))
 
-    # try with or without this
-    # runner.prompt = build_story()
+    checkpoint = prompt.get()
+    options = gen_options(prompt)
+    prompt.set(checkpoint)
 
-    checkpoint = runner.location()
-    options = runner.gen_options()
-    runner.rewind(checkpoint)
+    num_tokens = llm.get_num_tokens(prompt.get())
+    print(f"\n({num_tokens}) > ", end="")
+    prompt.text += "\n> "
 
-    print(f"\n({runner.spent_context}) > ", end="")
-    runner.add("\n> ", hide=True)
+    # Read user choice
+    while (choice := getch()) not in "123459cq":
+        pass
 
-    (event, value) = choose(options)
-    if event == "QUIT":
-        print("---------------------")
-        print(runner.prompt)
+    if choice == "q":
+        print("-----")
+        print(prompt.get())
         break
 
-    if event == "INPUT":
-        runner.add(value + "\n\n")
+    if choice == "5":
+        prompt.add(input("5. ") + "\n\n")
 
-    if event == "REGEN":
+    if choice in "4321":
+        prompt.add(options[int(choice) - 1] + "\n\n")
+
+    if choice == "9":
         print("(REGEN)\n")
-        total_story.pop()
-        runner.prompt = start_prompt
+        prompt.set(start_prompt)
 
-    if event == "SUMMARY":
+    if choice == "c":
         print("(SUMMARY)\n")
-        mytho_compress(runner.prompt)
+        print(mytho_compress(prompt.get()))
         print("-----\n")
-        total_story.pop()
-        runner.prompt = start_prompt
+        prompt.set(start_prompt)
--- rewrite.py
+++ rewrite.py
@@ -1,97 +1,5 @@
 from llm import llm, tokenize
 
-rpg_output = """
-You wake up dizzy, not quite sure what is going on. Your head hurts and you feel like you've been unconscious for a long time. Your surroundings are dimly lit by torches mounted high on the walls of an underground chamber. You sit up slowly, rubbing your eyes. As they adjust to the light, you see that the walls around you are smooth and polished, as if chiseled from solid rock.
-
-> Look around the room.
-
-You look around the room. Besides yourself, there is a human skeleton lying on the floor near you. There's also an ornately decorated wooden chest in front of you, its lid open and empty. Your hand feels for your sword but it's not there either!
-
-> Examine the skeleton.
-
-You examine the skeleton. It appears to be wearing fine leather armor. A dagger lies nearby, embedded in its ribcage. You notice bloodstains on the floor around the body.
-
-> Search the skeleton's body for anything useful.
-
-You search the skeleton's body and find a small pouch containing some gold coins and a key. The key seems to be about six inches long, with teeth carved into its end.
-
-> Take the key and leave this room.
-
-You take the key and carefully step out of the room. As you do so, you hear a low rumbling sound coming from somewhere beyond the wall ahead of you.
-
-> Continue exploring.
-
-As you start walking down the corridor, torches on either side grow brighter, casting more light upon the smooth granite walls. The noise grows louder and closer until suddenly a section of the wall disintegrates into dust, revealing a vast tunnel beyond.
-
-> Enter the tunnel.
-
-You enter the tunnel and see that it descends steeply into darkness. Ahead, you can make out what seems to be a large shadowy shape moving slowly toward you. As you watch, terrified, the light from your torch illuminates the creature more clearly: it's a gigantic spider, at least six feet across!
-
-> Try to bargain with the spider.
-
-You try to bargain with the spider but it seems more interested in eating you than striking any deals.
-
-> Attack the spider.
-
-You attack the spider but your blow glances off its tough exoskeleton. The creature reacts swiftly, rushing towards you and biting into your arm with its fangs. You feel a sharp sting of pain as it injects poison, numbing your limb.
-
-> Use magic if available
-
-You use a spell you learned to create a wall of fire between you and the spider. It screams in pain and retreats momentarily.
-
-> Attempt to escape
-
-You attempt to escape but the spider recovers quickly. As it moves towards you again, you notice that its legs are covered with shimmering webs. You realize these must be what made your blow glance off earlier.
-
-> Attack again, focusing on its legs this time
-
-You attack again, this time focusing on the spider's legs and managing to sever one of them. The creature recoils from the pain, screeching with fury.
-
-> Use magic if available
-
-You use another spell to create a wall of ice around you, ensuring that spider won't get close again. The spider tries desperately to break through but fails.
-
-> Continue down the tunnel
-
-You continue down the tunnel, leaving the injured spider behind. As you walk further into the cave, the floor begins to slope upward. At last you reach a fork in the tunnel where two tunnels meet: one leads off to your right and another goes straight ahead. A dim light glows from somewhere up ahead of the left tunnel.
-"""
-
-rpg_output2 = """
-You wake up dizzy, not quite sure what is going on. Your head hurts and you feel like you've been unconscious for a long time. Your surroundings are dimly lit by torches mounted high on the walls of an underground chamber. You sit up slowly, rubbing your eyes. As they adjust to the light, you see that the walls around you are smooth and polished, as if chiseled from solid rock.
-
-You look around the room. Besides yourself, there is a human skeleton lying on the floor near you. There's also an ornately decorated wooden chest in front of you, its lid open and empty. Your hand feels for your sword but it's not there either!
-
-You examine the skeleton. It appears to be wearing fine leather armor. A dagger lies nearby, embedded in its ribcage. You notice bloodstains on the floor around the body.
-
-You search the skeleton's body and find a small pouch containing some gold coins and a key. The key seems to be about six inches long, with teeth carved into its end.
-
-You take the key and carefully step out of the room. As you do so, you hear a low rumbling sound coming from somewhere beyond the wall ahead of you.
-
-As you start walking down the corridor, torches on either side grow brighter, casting more light upon the smooth granite walls. The noise grows louder and closer until suddenly a section of the wall disintegrates into dust, revealing a vast tunnel beyond.
-
-You enter the tunnel and see that it descends steeply into darkness. Ahead, you can make out what seems to be a large shadowy shape moving slowly toward you. As you watch, terrified, the light from your torch illuminates the creature more clearly: it's a gigantic spider, at least six feet across!
-
-You try to bargain with the spider but it seems more interested in eating you than striking any deals.
-
-You attack the spider but your blow glances off its tough exoskeleton. The creature reacts swiftly, rushing towards you and biting into your arm with its fangs. You feel a sharp sting of pain as it injects poison, numbing your limb.
-
-You use a spell you learned to create a wall of fire between you and the spider. It screams in pain and retreats momentarily.
-
-You attempt to escape but the spider recovers quickly. As it moves towards you again, you notice that its legs are covered with shimmering webs. You realize these must be what made your blow glance off earlier.
-
-You attack again, this time focusing on the spider's legs and managing to sever one of them. The creature recoils from the pain, screeching with fury.
-
-You use another spell to create a wall of ice around you, ensuring that spider won't get close again. The spider tries desperately to break through but fails.
-
-You continue down the tunnel, leaving the injured spider behind. As you walk further into the cave, the floor begins to slope upward. At last you reach a fork in the tunnel where two tunnels meet: one leads off to your right and another goes straight ahead. A dim light glows from somewhere up ahead of the left tunnel.
-"""
-
-characters = (
-    f"USER: Make a numbered list of all main actors in the following story.\n"
-    f"{rpg_output2}"
-    f"\nASSISTANT: Certainly"
-)
-
 
 def airo_compress(text):
     compress = (
@@ -126,7 +34,7 @@ def airo_compress(text):
 
 
 def mytho_compress(text):
-    while True:
+    for _ in range(5):
         compress = (
             f"### Instruction:\nSummarize the following story in a few paragraphs.\n"
             f"{text}"
@@ -154,12 +62,105 @@ def mytho_compress(text):
     return result
 
 
-mytho_compress(rpg_output2)
+if __name__ == "__main__":
+    rpg_output = """
+    You wake up dizzy, not quite sure what is going on. Your head hurts and you feel like you've been unconscious for a long time. Your surroundings are dimly lit by torches mounted high on the walls of an underground chamber. You sit up slowly, rubbing your eyes. As they adjust to the light, you see that the walls around you are smooth and polished, as if chiseled from solid rock.
 
-good_result = """
-You wake up in an underground chamber, disoriented and with a pounding headache. The room is illuminated by torches high on the walls. You see an empty chest and a human skeleton nearby. As you examine the body, you find some gold coins and a key in a pouch attached to it.
+    > Look around the room.
 
-You step out of the room into a corridor where torches grow brighter as you walk down it. A huge spider appears at the end of the tunnel, blocking your path. The creature seems more interested in eating you than negotiating with you and attacks when you try to bargain. You fight back but get poisoned by its fangs before retreating into a tunnel branching off from the main corridor.
+    You look around the room. Besides yourself, there is a human skeleton lying on the floor near you. There's also an ornately decorated wooden chest in front of you, its lid open and empty. Your hand feels for your sword but it's not there either!
 
-You use magic spells to create walls of fire and ice, successfully driving away the spider. As you continue down another tunnel, it slopes upward until you reach a fork where two tunnels meet: one leads right while another goes straight ahead. A dim light glows from somewhere up ahead on the left tunnel.
-"""
+    > Examine the skeleton.
+
+    You examine the skeleton. It appears to be wearing fine leather armor. A dagger lies nearby, embedded in its ribcage. You notice bloodstains on the floor around the body.
+
+    > Search the skeleton's body for anything useful.
+
+    You search the skeleton's body and find a small pouch containing some gold coins and a key. The key seems to be about six inches long, with teeth carved into its end.
+
+    > Take the key and leave this room.
+
+    You take the key and carefully step out of the room. As you do so, you hear a low rumbling sound coming from somewhere beyond the wall ahead of you.
+
+    > Continue exploring.
+
+    As you start walking down the corridor, torches on either side grow brighter, casting more light upon the smooth granite walls. The noise grows louder and closer until suddenly a section of the wall disintegrates into dust, revealing a vast tunnel beyond.
+
+    > Enter the tunnel.
+
+    You enter the tunnel and see that it descends steeply into darkness. Ahead, you can make out what seems to be a large shadowy shape moving slowly toward you. As you watch, terrified, the light from your torch illuminates the creature more clearly: it's a gigantic spider, at least six feet across!
+
+    > Try to bargain with the spider.
+
+    You try to bargain with the spider but it seems more interested in eating you than striking any deals.
+
+    > Attack the spider.
+
+    You attack the spider but your blow glances off its tough exoskeleton. The creature reacts swiftly, rushing towards you and biting into your arm with its fangs. You feel a sharp sting of pain as it injects poison, numbing your limb.
+
+    > Use magic if available
+
+    You use a spell you learned to create a wall of fire between you and the spider. It screams in pain and retreats momentarily.
+
+    > Attempt to escape
+
+    You attempt to escape but the spider recovers quickly. As it moves towards you again, you notice that its legs are covered with shimmering webs. You realize these must be what made your blow glance off earlier.
+
+    > Attack again, focusing on its legs this time
+
+    You attack again, this time focusing on the spider's legs and managing to sever one of them. The creature recoils from the pain, screeching with fury.
+
+    > Use magic if available
+
+    You use another spell to create a wall of ice around you, ensuring that spider won't get close again. The spider tries desperately to break through but fails.
+
+    > Continue down the tunnel
+
+    You continue down the tunnel, leaving the injured spider behind. As you walk further into the cave, the floor begins to slope upward. At last you reach a fork in the tunnel where two tunnels meet: one leads off to your right and another goes straight ahead. A dim light glows from somewhere up ahead of the left tunnel.
+    """
+
+    rpg_output2 = """
+    You wake up dizzy, not quite sure what is going on. Your head hurts and you feel like you've been unconscious for a long time. Your surroundings are dimly lit by torches mounted high on the walls of an underground chamber. You sit up slowly, rubbing your eyes. As they adjust to the light, you see that the walls around you are smooth and polished, as if chiseled from solid rock.
+
+    You look around the room. Besides yourself, there is a human skeleton lying on the floor near you. There's also an ornately decorated wooden chest in front of you, its lid open and empty. Your hand feels for your sword but it's not there either!
+
+    You examine the skeleton. It appears to be wearing fine leather armor. A dagger lies nearby, embedded in its ribcage. You notice bloodstains on the floor around the body.
+
+    You search the skeleton's body and find a small pouch containing some gold coins and a key. The key seems to be about six inches long, with teeth carved into its end.
+
+    You take the key and carefully step out of the room. As you do so, you hear a low rumbling sound coming from somewhere beyond the wall ahead of you.
+
+    As you start walking down the corridor, torches on either side grow brighter, casting more light upon the smooth granite walls. The noise grows louder and closer until suddenly a section of the wall disintegrates into dust, revealing a vast tunnel beyond.
+
+    You enter the tunnel and see that it descends steeply into darkness. Ahead, you can make out what seems to be a large shadowy shape moving slowly toward you. As you watch, terrified, the light from your torch illuminates the creature more clearly: it's a gigantic spider, at least six feet across!
+
+    You try to bargain with the spider but it seems more interested in eating you than striking any deals.
+
+    You attack the spider but your blow glances off its tough exoskeleton. The creature reacts swiftly, rushing towards you and biting into your arm with its fangs. You feel a sharp sting of pain as it injects poison, numbing your limb.
+
+    You use a spell you learned to create a wall of fire between you and the spider. It screams in pain and retreats momentarily.
+
+    You attempt to escape but the spider recovers quickly. As it moves towards you again, you notice that its legs are covered with shimmering webs. You realize these must be what made your blow glance off earlier.
+
+    You attack again, this time focusing on the spider's legs and managing to sever one of them. The creature recoils from the pain, screeching with fury.
+
+    You use another spell to create a wall of ice around you, ensuring that spider won't get close again. The spider tries desperately to break through but fails.
+
+    You continue down the tunnel, leaving the injured spider behind. As you walk further into the cave, the floor begins to slope upward. At last you reach a fork in the tunnel where two tunnels meet: one leads off to your right and another goes straight ahead. A dim light glows from somewhere up ahead of the left tunnel.
+    """
+
+    characters = (
+        f"USER: Make a numbered list of all main actors in the following story.\n"
+        f"{rpg_output2}"
+        f"\nASSISTANT: Certainly"
+    )
+
+    mytho_compress(rpg_output2)
+
+    good_result = """
+    You wake up in an underground chamber, disoriented and with a pounding headache. The room is illuminated by torches high on the walls. You see an empty chest and a human skeleton nearby. As you examine the body, you find some gold coins and a key in a pouch attached to it.
+
+    You step out of the room into a corridor where torches grow brighter as you walk down it. A huge spider appears at the end of the tunnel, blocking your path. The creature seems more interested in eating you than negotiating with you and attacks when you try to bargain. You fight back but get poisoned by its fangs before retreating into a tunnel branching off from the main corridor.
+
+    You use magic spells to create walls of fire and ice, successfully driving away the spider. As you continue down another tunnel, it slopes upward until you reach a fork where two tunnels meet: one leads right while another goes straight ahead. A dim light glows from somewhere up ahead on the left tunnel.
+    """
